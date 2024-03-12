3176e0786df2b0dfc9eecf0bcb58702808831f7e
boxgramer <boxgramer@gmail.com>
2024-03-12 10:58:55 +0800

    add signal

--- boxfirebaselib/src/main/java/com/boxgramer/boxfirebaselib/BoxFirebase.kt
+++ boxfirebaselib/src/main/java/com/boxgramer/boxfirebaselib/BoxFirebase.kt
@@ -1,6 +1,7 @@
 package com.boxgramer.boxfirebaselib
 
 import android.app.Activity
+import android.util.ArraySet
 import android.util.Log
 import android.view.View
 import android.widget.Toast
@@ -16,6 +17,7 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
+import org.godotengine.godot.plugin.SignalInfo
 import org.godotengine.godot.plugin.UsedByGodot
 import java.util.Date
 
@@ -34,6 +36,23 @@ class BoxFirebase(godot: Godot?) : GodotPlugin(godot) {
 //
     override fun onMainCreate(activity: Activity?): View? {
         return super.onMainCreate(activity)
+    }
+
+
+    override fun getPluginSignals(): MutableSet<SignalInfo> {
+        val signals= mutableSetOf<SignalInfo>()
+        signals.add(SignalInfo("onTestEmitSingal", String::class.java))
+        signals.add(SignalInfo("onSuccessCheckAuth", String::class.java))
+        signals.add(SignalInfo("onErrorCheckAuth", String::class.java))
+        signals.add(SignalInfo("onStartSaveData"))
+        signals.add(SignalInfo("onSuccessSaveData"))
+        signals.add(SignalInfo("onErrorSaveData", String::class.java))
+        signals.add(SignalInfo("onStartGetData"))
+        signals.add(SignalInfo("onSuccessGetData", String::class.java))
+        signals.add(SignalInfo("onErrorGetData", String::class.java))
+
+        return signals
+
     }
 
     @UsedByGodot
@@ -55,17 +74,23 @@ class BoxFirebase(godot: Godot?) : GodotPlugin(godot) {
                 ?.addOnCompleteListener {
                    Log.d("godot", "Sign-in anonymously: success")
                     currentUser = auth?.currentUser
+                    emitSignal("onSuccessCheckAuth", currentUser?.uid.toString())
                 }?.addOnFailureListener { e ->
                     Log.d("godot", "sing-in anonymously: failed ", e)
+                    emitSignal("onErrorCheckAuth", e.message)
                 }
         }
+
     }
+
 
     @UsedByGodot
     fun saveDataByUser(nameColletion:String, jsonSTR : String ) {
         Log.d(TAG, "start save data ${jsonSTR}")
+        emitSignal("onStartSaveData")
         if (currentUser == null )  {
             Log.d(TAG , "User Not Found")
+            emitSignal("onErrorSaveData", "User Not Found")
             return
         }
         val parser :Parser = Parser.default()
@@ -77,9 +102,12 @@ class BoxFirebase(godot: Godot?) : GodotPlugin(godot) {
             ?.set(user, SetOptions.merge())
             ?.addOnSuccessListener {
                 Log.d(TAG, "Save Data success")
+                emitSignal("onSuccessSaveData")
+
             }
             ?.addOnFailureListener { e ->
                 Log.d(TAG, "Save data failed", e)
+                emitSignal("onErrorSaveData", e.message)
             }
 
     }
@@ -88,8 +116,10 @@ class BoxFirebase(godot: Godot?) : GodotPlugin(godot) {
     fun getDataByUser(nameColletion: String) {
         Log.d(TAG , "start get data")
 
+        emitSignal("onStartGetData")
         if (currentUser == null )  {
             Log.d(TAG , "User Not Found")
+//            emitSignal("onErrorGetData", "User Not Found")
             return
         }
         dbFirestore?.collection(nameColletion)
@@ -99,12 +129,16 @@ class BoxFirebase(godot: Godot?) : GodotPlugin(godot) {
                if(doc != null) {
                    Log.d(TAG, "Data was Founded")
                    Log.d(TAG, doc.toString())
+                    emitSignal("onSuccessGetData", doc.toString()  )
                }else {
+                   val message = "Data Not found"
                    Log.d(TAG , "Data not found")
+                   emitSignal("onErrorGetData", message)
                }
             }
             ?.addOnFailureListener { e ->
                 Log.d(TAG, "failed retrive data ", e)
+//                emitSignal("onErrorGetData", e.message)
             }
 
     }
@@ -136,23 +170,12 @@ class BoxFirebase(godot: Godot?) : GodotPlugin(godot) {
                     Log.w("godot" , "Error getting documents . ", exception)
             }
         })
-//        runOnUiThread{
-////          Toast.makeText(activity , "test ", Toast.LENGTH_LONG).show()
-//            var db = Firebase.firestore
-//            Log.d("godot", "test firestore ")
-//        }
-//        db.collection("data")
-//            .get()
-//            .addOnSuccessListener{
-//                result->
-//                for (document in result) {
-//                    Log.d("box", "${document.id} => ${document.data}")
-//                }
-//            }
-//            .addOnFailureListener{
-//                    exception ->
-//                    Log.w("box" , "Error getting documents . ", exception)
-//            }
+    }
+    @UsedByGodot
+    fun testEmitSignal()  {
+        Log.d(TAG , "Start test Emit Signal")
+
+        emitSignal("onTestEmitSingal" , "hello from plugin android ")
     }
 
 
