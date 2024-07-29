384428067f0c61814b907231f3220af02487a31e
slabgames <55172860+slabgames@users.noreply.github.com>
2024-07-29 18:57:45 +0800

    Update build.gradle.kts

--- boxfirebaselib/build.gradle.kts
+++ boxfirebaselib/build.gradle.kts
@@ -46,8 +46,9 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.gms:play-services-games-v2:20.1.2")
     implementation("com.google.android.material:material:1.12.0")
-    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("com.google.firebase:firebase-auth-ktx")
@@ -55,7 +56,7 @@ dependencies {
     compileOnly(files("libs/godot-lib.3.5.3.stable.release.aar"))
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
 }
