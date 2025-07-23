07a971a54055132c29e4bad2144cd51b6f7846c8
slabgames <55172860+slabgames@users.noreply.github.com>
2025-07-23 14:51:46 +0800

    update version

--- boxfirebaselib/build.gradle.kts
+++ boxfirebaselib/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     //id("com.google.gms.google-services")
 
 }
-project.ext.set("version", "1.0.0")
+project.ext.set("version", "1.0.1")
 android {
     namespace = "com.boxgramer.boxfirebaselib"
     compileSdk = 34
@@ -44,15 +44,17 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
-    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-firestore")
-    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-inappmessaging")
+    implementation("com.google.firebase:firebase-inappmessaging-display")
     implementation("com.beust:klaxon:5.5")
-    compileOnly(files("libs/godot-lib.3.5.3.stable.release.aar"))
+    compileOnly(files("libs/godot-lib.3.6.stable.release.aar"))
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
